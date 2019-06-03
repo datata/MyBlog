e8c863957108acc1ee263014433d487bd4b132ae
datata <danitt15@gmail.com>
2019-06-03 20:03:39 +0200

    Finish debugger-logger feature

--- src/kernel.php
+++ src/kernel.php
@@ -3,7 +3,10 @@ namespace App;
 
 class kernel
 {
-    public function __construct(){       
+    public function __construct(){ 
+        $logManager = new LogManager();
+        $logManager ->info("Arrancando la aplicacion");
+        
         $viewManager = new ViewManager();
         $viewManager->renderTemplate("index.view.html");
     }
