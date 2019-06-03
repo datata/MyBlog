2825b9c396b22e816d220b0527a02a190c85010a
datata <danitt15@gmail.com>
2019-06-03 19:48:52 +0200

    Finish Twig Feature

--- src/kernel.php
+++ src/kernel.php
@@ -3,7 +3,10 @@ namespace App;
 
 class kernel
 {
-    public function __construct(){
-        echo "<h1>HOLA MUNDO</h1>";
+    public function __construct(){       
+        $viewManager = new ViewManager();
+        $viewManager->renderTemplate("index.view.html");
     }
 }
+
+                                 
